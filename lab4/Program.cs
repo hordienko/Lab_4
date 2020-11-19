024141b4139fb9e343a26e613aa2f1cd6ee3717c
Anna Hordiienko <annahordienko@yahoo.com>
2020-11-19 22:10:12 +0200

    first commit

---
+++ lab4/Program.cs
@@ -0,0 +1,88 @@
+﻿using lab4;
+using System;
+using System.IO;
+
+
+namespace Ocinka
+{
+   
+    class Program
+    {
+        
+       
+
+
+        static void Main(string[] args)
+        {
+
+            double[,] exp1= Read.Expert1();
+            double[,] exp2 = Read.Expert2();
+            double[,] exp3 = Read.Expert3();
+            double[,] exp4 = Read.Expert4();
+            string[] book = Read.Book();
+            string[] parametr = Read.Parametrs();
+            double[] weight = Read.GetWeight();
+
+            Console.WriteLine("Книги:");
+            for (int i = 0; i < book.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} {book[i]}");
+            }
+
+            Console.WriteLine("\n Необхiднi параметри :");
+            for (int i = 0; i < parametr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} {parametr[i]}");
+            }
+
+            Console.WriteLine("\nШкала оцiнювання: 1-5");
+            
+
+            double book1_genre =Math.Round(weight[0]*( exp1[0, 0] + exp2[0, 0] + exp3[0, 0] + exp4[0, 0]),1);
+            double book2_genre = Math.Round(weight[0] *( exp1[1, 0] + exp2[1, 0] + exp3[1, 0] + exp4[1, 0]), 1);
+            double book3_genre = Math.Round(weight[0] *( exp1[2, 0] + exp2[2, 0] + exp3[2, 0] + exp4[2, 0]), 1);
+            double book4_genre = Math.Round(weight[0] * (exp1[3, 0] + exp2[3, 0] + exp3[3, 0] + exp4[3, 0]), 1);
+            double book5_genre = Math.Round(weight[0] * (exp1[4, 0] + exp2[4, 0] + exp3[4, 0] + exp4[4, 0]), 1);
+
+            double book1_price = Math.Round(weight[1] * (exp1[0, 1] + exp2[0, 1] + exp3[0, 1] + exp4[0, 1]),1);
+            double book2_price = Math.Round(weight[1] * (exp1[1, 1] + exp2[1, 1] + exp3[1, 1] + exp4[1, 1]), 1);
+            double book3_price = Math.Round(weight[1] * (exp1[2, 1] + exp2[2, 1] + exp3[2, 1] + exp4[2, 1]), 1);
+            double book4_price = Math.Round(weight[1] * (exp1[3, 1] + exp2[3, 1] + exp3[3, 1] + exp4[3, 1]), 1);
+            double book5_price = Math.Round(weight[1] * (exp1[4, 1] + exp2[4, 1] + exp3[4, 1] + exp4[4, 1]), 1);
+
+            double book1_cover = Math.Round(weight[2] * (exp1[0, 2] + exp2[0, 2] + exp3[0, 2] + exp4[0, 2]), 1);
+            double book2_cover = Math.Round(weight[2] * (exp1[1, 2] + exp2[1, 2] + exp3[1, 2] + exp4[1, 1]), 1);
+            double book3_cover = Math.Round(weight[2] * (exp1[2, 2] + exp2[2, 2] + exp3[2, 2] + exp4[2, 2]), 1);
+            double book4_cover = Math.Round(weight[2] * (exp1[3, 2] + exp2[3, 2] + exp3[3, 2] + exp4[3, 2]), 1);
+            double book5_cover = Math.Round(weight[2] * (exp1[4, 2] + exp2[4, 2] + exp3[4, 2] + exp4[4, 2]), 1);
+
+            double book1_rating = Math.Round(weight[3] * (exp1[0, 3] + exp2[0, 3] + exp3[0, 3] + exp4[0, 3]), 1);
+            double book2_rating= Math.Round(weight[3] * (exp1[1, 3] + exp2[1, 3] + exp3[1, 3] + exp4[1, 3]), 1);
+            double book3_rating= Math.Round(weight[3] * (exp1[2, 3] + exp2[2, 3] + exp3[2, 3] + exp4[2, 3]), 1);
+            double book4_rating= Math.Round(weight[3] * (exp1[3, 3] + exp2[3, 3] + exp3[3, 3] + exp4[3, 3]), 1);
+            double book5_rating= Math.Round(weight[3] * (exp1[4, 3] + exp2[4, 3] + exp3[4, 3] + exp4[4, 3]), 1);
+
+            double book1_sum = Math.Round(book1_genre + book1_price + book1_cover + book1_rating,1);
+            double book2_sum = Math.Round( book2_genre + book2_price + book2_cover + book2_rating, 1);
+            double book3_sum = Math.Round(book3_genre + book3_price + book3_cover + book3_rating, 1);
+            double book4_sum = Math.Round(book4_genre + book4_price + book4_cover + book4_rating, 1);
+            double book5_sum = Math.Round(book5_genre + book5_price + book5_cover + book5_rating, 1);
+
+            Console.WriteLine("Результат");
+
+            Console.WriteLine("№ Параметр      |  Вага  |  Звiянi вiтром  |  Вино з кульбаб  |  Немає виходу  |  Володар перснiв  |  Ловець невинних душ  | ");
+            Console.WriteLine($"1 Жанр          |  {weight[0]}       {book1_genre}                {book2_genre}                 {book3_genre}               {book4_genre}                  {book5_genre} ");
+            Console.WriteLine($"2 Цiна          |  {weight[1]}       {book1_price}                {book2_price}                 {book3_price}             {book4_price}                {book5_price} ");
+            Console.WriteLine($"3 Обкладинка    |  {weight[2]}       {book1_cover}                  {book2_cover}                 {book3_cover}             {book4_cover}                {book5_cover}");
+            Console.WriteLine($"4 Рейтинг       |  {weight[3]}       {book1_rating}                {book2_rating}                   {book3_rating}               {book4_rating}                {book5_rating}");
+            Console.WriteLine($"Сума            |            {book1_sum}               {book2_sum}                {book3_sum}            {book4_sum}               {book5_sum} ");
+
+
+
+        }
+       
+
+
+
+    }
+}
---
+++ lab4/Read.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace lab4
+{
+    class Read
+    {
+        static double[,] exp1;
+        static double[,] exp2;
+        static double[,] exp3;
+        static double[,] exp4;
+        static string[] book;
+        static string[] parametr;
+        static double[] weight;
+        public static double[,] Expert1() // метод для зчитування і запису чисел в масив 
+        {
+            string filePath = Path.GetFullPath("1.txt");
+            int y = 0;
+            using var fileReader = new StreamReader(filePath);
+            string line;
+            exp1 = new double[5, 4];
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                string[] lines = line.Split(' ');
+                double[] mas = new double[4];
+
+                for (int i = 0; i < 4; i++)
+                {
+                    exp1[y, i] = Convert.ToDouble(lines[i]);
+                }
+                y++;
+            }
+            return exp1;
+        }
+
+
+        public static double[,] Expert2()  
+        {
+            string filePath = Path.GetFullPath("2.txt");
+            int y = 0;
+            using var fileReader = new StreamReader(filePath);
+            string line;
+            exp2 = new double[5, 4];
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                string[] lines = line.Split(' ');
+                double[] mas = new double[4];
+
+                for (int i = 0; i < 4; i++)
+                {
+                    exp2[y, i] = Convert.ToDouble(lines[i]);
+                }
+                y++;
+            }
+            return exp2;
+        }
+
+        public static double[,] Expert3() 
+        {
+            string filePath = Path.GetFullPath("3.txt");
+            int y = 0;
+            using var fileReader = new StreamReader(filePath);
+            string line;
+            exp3 = new double[5, 4];
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                string[] lines = line.Split(' ');
+                double[] mas = new double[4];
+
+                for (int i = 0; i < 4; i++)
+                {
+                    exp3[y, i] = Convert.ToDouble(lines[i]);
+                }
+                y++;
+            }
+            return exp3;
+        }
+
+        public static double[,] Expert4() 
+        {
+            string filePath = Path.GetFullPath("4.txt");
+            int y = 0;
+            using var fileReader = new StreamReader(filePath);
+            string line;
+            exp4 = new double[5, 4];
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                string[] lines = line.Split(' ');
+                double[] mas = new double[4];
+
+                for (int i = 0; i < 4; i++)
+                {
+                    exp4[y, i] = Convert.ToDouble(lines[i]);
+                }
+                y++;
+            }
+            return exp4;
+        }
+
+
+        public static string[] Book() 
+        {
+            string filePath = Path.GetFullPath("book.txt");
+
+            using var fileReader = new StreamReader(filePath);
+            string file = fileReader.ReadToEnd();
+            string[] lines = file.Split('\n');
+
+
+            book = new string[lines.Length];
+
+            for (int i = 0; i < book.Length; i++)
+            {
+                book[i] = lines[i];
+            }
+            return book;
+        }
+
+        public static string[] Parametrs() 
+        {
+            string filePath = Path.GetFullPath("param.txt");
+
+            using var fileReader = new StreamReader(filePath);
+            string file = fileReader.ReadToEnd();
+            string[] lines = file.Split('\n');
+
+
+            parametr = new string[lines.Length];
+
+            for (int i = 0; i < parametr.Length; i++)
+            {
+                parametr[i] = lines[i];
+            }
+            return parametr;
+        }
+        public static double[] GetWeight() // метод для зчитування і запису чисел в масив 
+        {
+            string filePath = Path.GetFullPath("Weight.txt");
+
+            using var fileReader = new StreamReader(filePath);
+            string file = fileReader.ReadToEnd();
+            string[] lines = file.Split('\n');
+
+
+            weight = new double[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                weight[i] = Convert.ToDouble(lines[i]);
+                //Console.WriteLine($"{i} {fileProp[i]}");
+            }
+            return weight;
+        }
+    }
+}
